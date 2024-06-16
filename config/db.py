3e41b01d309a59ea8b2d615896c801161e4c4c48
dimact22 <90906931+dimact22@users.noreply.github.com>
2024-06-16 17:52:08 +0200

    Update db.py

--- config/db.py
+++ config/db.py
@@ -4,12 +4,10 @@ import os
 
 app = FastAPI()
 
-PASS = "admin"
-
-mongo_uri = os.getenv(
-    "MONGO_URL", f"mongodb+srv://admin:{PASS}@cluster0.eekni1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongo_uri = os.getenv("MONGO_URL", "mongodb://localhost:27017/redilab")
 print("MongoDB Verbindungs-URL:", mongo_uri)
 client = MongoClient(mongo_uri)
 
 db = client.get_database("redilab")
 collection = db.get_collection("users")
+
